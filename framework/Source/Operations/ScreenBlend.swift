63ab9eeeb3f1ac64e4f51874667e053af58bc42d
yyjim <yy.jim731@gmail.com>
2023-11-01 22:42:38 +0800

    :sparkles: Enhancement ScreenBlend with opacity and content mode

--- framework/Source/Operations/ScreenBlend.swift
+++ framework/Source/Operations/ScreenBlend.swift
@@ -1,5 +1,12 @@
 public class ScreenBlend: BasicOperation {
+    public var opacity: Float = 1 { didSet { uniformSettings["opacity"] = opacity } }
+    public var mode: Float = 0 { didSet { uniformSettings["mode"] = mode } }
+
     public init() {
         super.init(fragmentFunctionName:"screenBlendFragment", numberOfInputs:2)
+        ({
+            opacity = 1
+            mode = 0
+        })()
     }
 }
