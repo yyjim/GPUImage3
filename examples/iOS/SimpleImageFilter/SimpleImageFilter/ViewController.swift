1a26aff1ad2221f4053b3efaba1dc449cdf2bc01
yyjim <yy.jim731@gmail.com>
2024-03-29 09:08:46 +0800

    :sparkles: Update SimpleImageFilter demo

--- examples/iOS/SimpleImageFilter/SimpleImageFilter/ViewController.swift
+++ examples/iOS/SimpleImageFilter/SimpleImageFilter/ViewController.swift
@@ -5,30 +5,28 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var renderView: RenderView!
 
-    var picture:PictureInput!
-    var filter:SaturationAdjustment!
-    
+    var picture: PictureInput!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        renderView.contentMode = .scaleAspectFit
+        renderView.colorPixelFormat = .bgra8Unorm
+    }
+
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
-        // Filtering image for saving
-        let testImage = UIImage(named:"WID-small.jpg")!
-        let toonFilter = ToonFilter()
-        let filteredImage = testImage.filterWithOperation(toonFilter)
-        
-        let pngImage = UIImagePNGRepresentation(filteredImage)!
-        do {
-            let documentsDir = try FileManager.default.url(for:.documentDirectory, in:.userDomainMask, appropriateFor:nil, create:true)
-            let fileURL = URL(string:"test.png", relativeTo:documentsDir)!
-            try pngImage.write(to:fileURL, options:.atomic)
-        } catch {
-            print("Couldn't write to file with error: \(error)")
-        }
-        
-        // Filtering image for display
-        picture = PictureInput(image:UIImage(named:"WID-small.jpg")!)
-        filter = SaturationAdjustment()
-        picture --> filter --> renderView
+
+        applyGrain()
+    }
+
+    private lazy var grainFilter = Grain()
+    private func applyGrain() {
+        let grainFilter = Grain()
+        grainFilter.strength = 9.0 / 100.0
+        grainFilter.time = 1.1
+
+        picture = PictureInput(image: UIImage(named:"WID-small.jpg")!)
+        picture --> grainFilter --> renderView
         picture.processImage()
     }
 }
