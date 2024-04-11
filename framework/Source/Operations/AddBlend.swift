9808db00adc473b9952560b0165294a62764eda9
Chiaote Ni <aaron.chiao.19@gmail.com>
2024-04-11 17:47:29 +0800

    feat: created AddBlendUniform to support opacity adjustments

--- framework/Source/Operations/AddBlend.swift
+++ framework/Source/Operations/AddBlend.swift
@@ -1,6 +1,11 @@
 public class AddBlend: BasicOperation {
-    
+
+    public var opacity: Float = 1 { didSet { uniformSettings["opacity"] = opacity } }
+
     public init() {
         super.init(fragmentFunctionName:"addBlendFragment", numberOfInputs:2)
+        ({
+            opacity = 1
+        })()
     }
 }
