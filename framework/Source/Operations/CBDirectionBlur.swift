689e6daa8cd111c604b1c41bb6478fbfef0f7100
yyjim <yy.jim731@gmail.com>
2024-04-22 19:25:26 +0800

    :sparkles: Add CBDirectionBlur

---
+++ framework/Source/Operations/CBDirectionBlur.swift
@@ -0,0 +1,29 @@
+//
+//  DirectionBlur.swift
+//  GPUImage
+//
+//  Created by Jim Wang on 2024/3/29.
+//  Copyright © 2024 Red Queen Coder, LLC. All rights reserved.
+//
+
+import Foundation
+
+public class CBDirectionBlur: BasicOperation {
+    // 0.0 ~ 360.0
+    public var degree: Float = 45.0 {
+        didSet {
+            let radian =  degree / 180.0 * Float.pi
+            uniformSettings["radian"] = radian
+        }
+    }
+    // 0.0 ~ 1.0
+    public var length: Float = 0.15 { didSet { uniformSettings["length"] = length } }
+
+    public init() {
+        super.init(fragmentFunctionName:"directionBlurFragment", numberOfInputs:1)
+        ({
+            degree = 45.0
+            length = 0.15
+        })()
+    }
+}
