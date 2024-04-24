2477ec12462ff23eb474e9c3a5ad01c378a28e11
Chiaote Ni <aaron.chiao.19@gmail.com>
2024-04-24 18:13:09 +0800

    :sparkles: update demo vc

--- examples/iOS/SimpleImageFilter/SimpleImageFilter/ViewController.swift
+++ examples/iOS/SimpleImageFilter/SimpleImageFilter/ViewController.swift
@@ -7,6 +7,7 @@ class ViewController: UIViewController {
 
     var picture: PictureInput!
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
         renderView.contentMode = .scaleAspectFit
@@ -21,12 +22,26 @@ class ViewController: UIViewController {
 
     private lazy var grainFilter = Grain()
     private func applyGrain() {
-        let grainFilter = Grain()
-        grainFilter.strength = 9.0 / 100.0
-        grainFilter.time = 1.1
+//        let grainFilter = Grain()
+//        grainFilter.strength = 9.0 / 100.0
+//        grainFilter.time = 1.1
+
+        let filter = CBPerlineNoise()//Kirakira()
+
+//        let filter = CBKirakiraLightExtractor()
+//        let input2 = PictureInput(image: UIImage(named:"overlay-1.jpg")!)
+//        input2.addTarget(filter, atTargetIndex: 1)
+//        input2.processImage()
 
         picture = PictureInput(image: UIImage(named:"WID-small.jpg")!)
-        picture --> grainFilter --> renderView
+
+//        picture --> filter --> blendImage
+
+//        let linearDodge = AddBlend()
+//        blendImage.addTarget(linearDodge, atTargetIndex: 1)
+//        blendImage.processImage()
+
+        picture --> filter --> renderView
         picture.processImage()
     }
 }
