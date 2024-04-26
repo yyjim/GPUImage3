2d6b0f9a169584ff7bd889ba7d994d249ec4bcf2
Chiaote Ni <aaron.chiao.19@gmail.com>
2024-04-26 15:46:25 +0800

    :sparkles: test kirakira effect in simpleImageFilter

--- examples/iOS/SimpleImageFilter/SimpleImageFilter/ViewController.swift
+++ examples/iOS/SimpleImageFilter/SimpleImageFilter/ViewController.swift
@@ -26,7 +26,25 @@ class ViewController: UIViewController {
 //        grainFilter.strength = 9.0 / 100.0
 //        grainFilter.time = 1.1
 
-        let filter = Kirakira()
+        let filter = {
+            let filter = Kirakira(rayCount: 3)
+            //        filter.equalMinHue = 0.54
+            //        filter.equalMaxHue = 0
+            //        filter.equalSaturation = 0.15
+            //        filter.equalBrightness = 2.8
+            //        filter.rayLength = 0.5
+            //        filter.sparkleExposure = 0.1
+            //        filter.blur = 0
+            //        filter.colorMode = .random
+            //        filter.saturation = 0.5
+            //        filter.centerSaturation = 0.3
+            //        filter.minHue = 0
+            //        filter.maxHue = 1
+            //        filter.noiseInfluence = 1
+            //        filter.increasingRate = 0.03
+            //        filter.sparkleAmount = 0.6
+            return filter
+        }()
 
 //        let filter = CBKirakiraLightExtractor()
 //        let input2 = PictureInput(image: UIImage(named:"overlay-1.jpg")!)
