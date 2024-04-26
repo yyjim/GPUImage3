3575c11102849ae003570b6ace36d5d9a7ecefe3
Chiaote Ni <aaron.chiao.19@gmail.com>
2024-04-26 22:26:43 +0800

    :sparkles: clean up the demo viewController

--- examples/iOS/SimpleImageFilter/SimpleImageFilter/ViewController.swift
+++ examples/iOS/SimpleImageFilter/SimpleImageFilter/ViewController.swift
@@ -17,49 +17,41 @@ class ViewController: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
 
-        applyGrain()
+        applyKiraKira()
     }
 
-    private lazy var grainFilter = Grain()
     private func applyGrain() {
-//        let grainFilter = Grain()
-//        grainFilter.strength = 9.0 / 100.0
-//        grainFilter.time = 1.1
+        let grainFilter = Grain()
+        grainFilter.strength = 9.0 / 100.0
+        grainFilter.time = 1.1
 
+        picture = PictureInput(image: UIImage(named:"WID-small.jpg")!)
+        picture --> grainFilter --> renderView
+        picture.processImage()
+    }
+
+    private func applyKiraKira() {
         let filter = {
             let filter = Kirakira(rayCount: 3)
-            //        filter.equalMinHue = 0.54
-            //        filter.equalMaxHue = 0
-            //        filter.equalSaturation = 0.15
-            //        filter.equalBrightness = 2.8
-            //        filter.rayLength = 0.5
-            //        filter.sparkleExposure = 0.1
-            //        filter.blur = 0
-            //        filter.colorMode = .random
-            //        filter.saturation = 0.5
-            //        filter.centerSaturation = 0.3
-            //        filter.minHue = 0
-            //        filter.maxHue = 1
-            //        filter.noiseInfluence = 1
-            //        filter.increasingRate = 0.03
-            //        filter.sparkleAmount = 0.6
+            filter.equalMinHue = 0.54
+            filter.equalMaxHue = 0
+            filter.equalSaturation = 0.15
+            filter.equalBrightness = 2.8
+            filter.rayLength = 0.5
+            filter.sparkleExposure = 0.1
+            filter.blur = 0
+            filter.colorMode = .random
+            filter.saturation = 0.5
+            filter.centerSaturation = 0.3
+            filter.minHue = 0
+            filter.maxHue = 1
+            filter.noiseInfluence = 1
+            filter.increasingRate = 0.03
+            filter.sparkleAmount = 0.6
             return filter
         }()
 
-//        let filter = CBKirakiraLightExtractor()
-//        let input2 = PictureInput(image: UIImage(named:"overlay-1.jpg")!)
-//        input2.addTarget(filter, atTargetIndex: 1)
-//        input2.processImage()
-
-//        picture = PictureInput(image: UIImage(named:"overlay-3.png")!)
         picture = PictureInput(image: UIImage(named:"WID-small.jpg")!)
-
-//        picture --> filter --> blendImage
-
-//        let linearDodge = AddBlend()
-//        blendImage.addTarget(linearDodge, atTargetIndex: 1)
-//        blendImage.processImage()
-
         picture --> filter --> renderView
         picture.processImage()
     }
