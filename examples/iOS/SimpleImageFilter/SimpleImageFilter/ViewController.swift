7c48eab98bd72d89b5f0e4d9b522d3eb850d33da
ChiaoteNi <aaron.chiao.19@gmail.com>
2024-05-06 10:16:36 +0800

    :recycle: codable parameters for the kirakira effect (#7)
    * :recycle: codable parameters for the kirakira effect
    * :sparkles: support to decode from JSON
    * :bug: set up the properties to the sub-effects
    * :sparkles: simplify the Demo viewController
    * :recycle: refactor the decoding process
    * :recycle: remove unused constructor
    * :recycle: modify the names
    * :bug: correct the decoding process of the kirakira effect

--- examples/iOS/SimpleImageFilter/SimpleImageFilter/ViewController.swift
+++ examples/iOS/SimpleImageFilter/SimpleImageFilter/ViewController.swift
@@ -30,25 +30,22 @@ class ViewController: UIViewController {
     }
 
     private func applyKiraKira() {
-        let filter = {
-            let filter = Kirakira(rayCount: 3)
-            filter.equalMinHue = 0.54
-            filter.equalMaxHue = 0
-            filter.equalSaturation = 0.15
-            filter.equalBrightness = 2.8
-            filter.rayLength = 0.5
-            filter.sparkleExposure = 0.1
-            filter.blur = 0
-            filter.colorMode = .random
-            filter.saturation = 0.5
-            filter.centerSaturation = 0.3
-            filter.minHue = 0
-            filter.maxHue = 1
-            filter.noiseInfluence = 1
-            filter.increasingRate = 0.03
-            filter.sparkleAmount = 0.6
-            return filter
+        let jsonFileNames = [
+            "Kirakira_Flashlight",
+            "Kirakira_Diamond",
+            "Kirakira_Rainbow",
+            "Kirakira_Barbie",
+            "Kirakira_Golden",
+            "Kirakira_Glamour"
+        ]
+        let parameters = { () -> Kirakira.Parameters in
+            let jsonFileName = jsonFileNames[1] + ".json"
+            let jsonData = try! Data(contentsOf: Bundle.main.url(forResource: jsonFileName, withExtension: nil)!)
+            let parameters = try! Kirakira.Parameters(with: jsonData)
+            return parameters
         }()
+
+        let filter = Kirakira(with: parameters)
 
         picture = PictureInput(image: UIImage(named:"WID-small.jpg")!)
         picture --> filter --> renderView
--- framework/Source/Kirakira.swift
+++ framework/Source/Kirakira.swift
@@ -10,9 +10,76 @@ import Foundation
 
 public class Kirakira: OperationGroup {
 
-    public enum ColorMode: Int {
+    public enum ColorMode: Int, Codable {
         case white = 0
         case random = 1
+    }
+
+    public struct Parameters {
+        public var colorMode: ColorMode
+        public var saturation: Float
+        public var centerSaturation: Float
+        public var equalMinHue: Float
+        public var equalMaxHue: Float
+        public var equalSaturation: Float
+        public var equalBrightness: Float
+        public var speed: Float
+        public var rayCount: Int
+        public var rayLength: Float
+        public var startAngle: Int
+        public var sparkleExposure: Float
+        public var minHue: Float
+        public var maxHue: Float
+        public var noiseInfluence: Float
+        public var increasingRate: Float
+        public var sparkleScale: Float
+        public var sparkleAmount: Float
+        public var frameRate: Float
+        public var blur: Int
+
+        public init(
+            colorMode: ColorMode = .random,
+            saturation: Float = 0.3,
+            centerSaturation: Float = 0.3,
+            equalMinHue: Float = 0.75,
+            equalMaxHue: Float = 0.083,
+            equalSaturation: Float = 0.15,
+            equalBrightness: Float = 2.0,
+            speed: Float = 7.5,
+            rayCount: Int = 2,
+            rayLength: Float = 0.08,
+            startAngle: Int = 45,
+            sparkleExposure: Float = 0.0,
+            minHue: Float = 0.0,
+            maxHue: Float = 1.0,
+            noiseInfluence: Float = 1.0,
+            increasingRate: Float = 0.3,
+            sparkleScale: Float = 0.7,
+            sparkleAmount: Float = 0.4,
+            frameRate: Float = 60,
+            blur: Int = 0
+        ) {
+            self.colorMode = colorMode
+            self.saturation = saturation
+            self.centerSaturation = centerSaturation
+            self.equalMinHue = equalMinHue
+            self.equalMaxHue = equalMaxHue
+            self.equalSaturation = equalSaturation
+            self.equalBrightness = equalBrightness
+            self.speed = speed
+            self.rayCount = rayCount
+            self.rayLength = rayLength
+            self.startAngle = startAngle
+            self.sparkleExposure = sparkleExposure
+            self.minHue = minHue
+            self.maxHue = maxHue
+            self.noiseInfluence = noiseInfluence
+            self.increasingRate = increasingRate
+            self.sparkleScale = sparkleScale
+            self.sparkleAmount = sparkleAmount
+            self.frameRate = frameRate
+            self.blur = blur
+        }
     }
 
     // MARK: Properties
@@ -89,30 +156,32 @@ public class Kirakira: OperationGroup {
     private let saturationEffect = SaturationAdjustment()
     private let addBlend = AddBlend()
 
-    public init(rayCount: Int = 2) {
-        self.rayCount = rayCount
-        self.sparklesEffect = Sparkles(rayCount: rayCount)
+    public init(with parameters: Parameters) {
+        self.rayCount = parameters.rayCount
+        self.sparklesEffect = Sparkles(rayCount: parameters.rayCount)
         super.init()
 
-        ({equalMinHue = 0.75})()
-        ({equalMaxHue = 0.083})()
-        ({equalSaturation = 0.15})()
-        ({equalBrightness = 2.0})()
-        ({speed = 7.5})()
-        ({rayLength = 0.08})()
-        ({sparkleExposure = 0.0})()
-        ({blur = 0})()
-        ({colorMode = .random})()
-        ({saturation = 0.3})()
-        ({minHue = 0.0})()
-        ({maxHue = 1.0})()
-        ({noiseInfluence = 1.0})()
-        ({increasingRate = 0.3})()
-        ({startAngle = 45})()
-        ({sparkleScale = 0.7})()
-        ({sparkleAmount = 0.4})()
-        ({frameRate = 60})()
-        ({centerSaturation = 0.3})()
+        ({
+            colorMode = parameters.colorMode
+            saturation = parameters.saturation
+            centerSaturation = parameters.centerSaturation
+            equalMinHue = parameters.equalMinHue
+            equalMaxHue = parameters.equalMaxHue
+            equalSaturation = parameters.equalSaturation
+            equalBrightness = parameters.equalBrightness
+            speed = parameters.speed
+            rayLength = parameters.rayLength
+            startAngle = parameters.startAngle
+            sparkleExposure = parameters.sparkleExposure
+            minHue = parameters.minHue
+            maxHue = parameters.maxHue
+            noiseInfluence = parameters.noiseInfluence
+            increasingRate = parameters.increasingRate
+            sparkleScale = parameters.sparkleScale
+            sparkleAmount = parameters.sparkleAmount
+            frameRate = parameters.frameRate
+            blur = parameters.blur
+        })()
 
         self.configureGroup { input, output in
             input
@@ -135,5 +204,79 @@ private extension Kirakira {
 
     func updateSparkleSaturation() {
         sparklesEffect.centerSaturation = centerSaturation * Float(colorMode.rawValue)
+    }
+}
+
+public extension Kirakira.Parameters {
+
+    init(with jsonData: Data) throws {
+        let decoder = JSONDecoder()
+        let parameters = try decoder.decode(Kirakira.Parameters.self, from: jsonData)
+        self = parameters
+    }
+}
+
+extension Kirakira.Parameters: Decodable {
+
+    fileprivate enum CodingKeys: String, CodingKey {
+        case equalMinHue
+        case equalMaxHue
+        case equalSaturation
+        case equalBrightness
+        case speed
+        case rayCount
+        case rayLength
+        case sparkleExposure
+        case blur
+        case colorMode
+        case saturation
+        case centerSaturation
+        case minHue
+        case maxHue
+        case noiseInfluence
+        case increasingRate
+        case startAngle
+        case sparkleAmount
+        case frameRate
+        case sparkleScale
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        equalMinHue = try container.decodeParamValue(Float.self, forKey: .equalMinHue)
+        equalMaxHue = try container.decodeParamValue(Float.self, forKey: .equalMaxHue)
+        equalSaturation = try container.decodeParamValue(Float.self, forKey: .equalSaturation)
+        equalBrightness = try container.decodeParamValue(Float.self, forKey: .equalBrightness)
+        speed = try container.decodeParamValue(Float.self, forKey: .speed)
+        rayCount = try container.decodeParamValue(Int.self, forKey: .rayCount)
+        rayLength = try container.decodeParamValue(Float.self, forKey: .rayLength)
+        sparkleExposure = try container.decodeParamValue(Float.self, forKey: .sparkleExposure)
+        blur = try container.decodeParamValue(Int.self, forKey: .blur)
+        colorMode = try container.decodeParamValue(Kirakira.ColorMode.self, forKey: .colorMode)
+        saturation = try container.decodeParamValue(Float.self, forKey: .saturation)
+        centerSaturation = try container.decodeParamValue(Float.self, forKey: .centerSaturation)
+        minHue = try container.decodeParamValue(Float.self, forKey: .minHue)
+        maxHue = try container.decodeParamValue(Float.self, forKey: .maxHue)
+        noiseInfluence = try container.decodeParamValue(Float.self, forKey: .noiseInfluence)
+        increasingRate = try container.decodeParamValue(Float.self, forKey: .increasingRate)
+        startAngle = try container.decodeParamValue(Int.self, forKey: .startAngle)
+        sparkleAmount = try container.decodeParamValue(Float.self, forKey: .sparkleAmount)
+        frameRate = try container.decodeParamValue(Float.self, forKey: .frameRate)
+        sparkleScale = try container.decodeParamValue(Float.self, forKey: .sparkleScale)
+    }
+}
+
+private extension KeyedDecodingContainer<Kirakira.Parameters.CodingKeys> {
+
+    enum ParamValueKeys: String, CodingKey {
+        case value
+    }
+
+    func decodeParamValue<T: Decodable>(
+        _ type: T.Type,
+        forKey key: Kirakira.Parameters.CodingKeys
+    ) throws -> T {
+        try nestedContainer(keyedBy: ParamValueKeys.self, forKey: key)
+            .decode(T.self, forKey: .value)
     }
 }
